9853318b5ae18975e8f9430c83a18ba0d8a3098b
LucasArauj99 <lucassantiago830@gmail.com>
2023-12-23 01:27:49 -0300

    deixando o codigo mais limpo

--- previsao-do-tempo/main.py
+++ previsao-do-tempo/main.py
@@ -9,30 +9,37 @@ lon = -34.8813
 
 url1 = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={tokenn}"
 
+def converso_de_temperatuda(temperatura):
+    #transforma kelvin para celsius
+    return round((temperatura - 273.15), 2)
+
+
+def temperatura_atual(): 
+    temperatura_atual = converso_de_temperatuda(dados['current']['temp'])
+    #temperatura_em_ceusios = temperatura_atual - 273.15
+    print(f"Temperatura Atual: {temperatura_atual}°C")
+    
+def previsao():
+    descricao_tempo_amanha = dados['daily'][1]['weather'][0]['description']
+    temp_minima = converso_de_temperatuda(dados['daily'][1]['temp']['min'])
+    temp_maxima = converso_de_temperatuda(dados['daily'][1]['temp']['max'])
+    translator = Translator()
+    descricao_traduzida = translator.translate(descricao_tempo_amanha, dest='pt').text
+    print(f"Previsão do tempo para amanhã: \n{descricao_traduzida},"
+         f"com a temperatura podendo variar de {temp_minima}°C até {temp_maxima}°C")
+
 try:
     # Fazendo a solicitação GET
-    response = requests.get(url= url1)
+    response = requests.get(url=url1)
 
     # Verificando se a solicitação foi bem-sucedida (código de status 200)
     if response.status_code == 200:
-        # Se a resposta estiver em formato JSON, você pode acessar os dados assim:
         dados = response.json()
-        temperatura_atual = dados['current']['temp']
-        temperatura_em_ceusios = temperatura_atual - 273.15
-        print(f"Temperatura Atual: {round(temperatura_em_ceusios, 2)}°C")
+        temperatura_atual()
 
         # Obtendo a primeira previsão
-        primeira_previsao = dados['daily'][0]['temp']
-        print(f"Primeira Previsão: {primeira_previsao}°C")
-
-        descricao_tempo_amanha = dados['daily'][1]['weather'][0]['description']
-        print(f"Previsão do Tempo para Amanhã: {descricao_tempo_amanha}")
-
-        
-        translator = Translator()
-        descricao_traduzida = translator.translate(descricao_tempo_amanha, dest='pt')
-        print(f"Tradução: {descricao_traduzida.text}")
-        
+        #primeira_previsao = dados['daily'][0]['temp']
+        previsao()
     else:
         print("Erro na solicitação. Código de status:", response.status_code)
 
